f5c280a1066e47f139b93258f649831148bc4375
John Fairhurst <johnfairh@gmail.com>
2018-07-29 15:48:52 +0100

    Add image transformer for core data

---
+++ TMLPresentation/Utils/CoreDataHelpers.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataHelpers.swift
+//  TMLPresentation
+//
+//  Distributed under the MIT license, see LICENSE.
+//
+
+import Foundation
+
+//
+// ValueTransformer to store images as JPEGs in the database
+//
+public final class ImageTransformer : ValueTransformer {
+    public override class func transformedValueClass() -> AnyClass {
+        return UIImage.self
+    }
+
+    public override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+    public override func transformedValue(_ value: Any?) -> Any? {
+        guard let image = value as? UIImage else {
+            Log.fatal("Image transformer confused")
+        }
+        return image.jpegData(compressionQuality: 1.0)
+    }
+
+    public override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else {
+            Log.fatal("Image transformer confused")
+        }
+        return UIImage(data: data)
+    }
+
+    /// One-time step
+    public static func install() {
+        ValueTransformer.setValueTransformer(ImageTransformer(), forName: NSValueTransformerName(rawValue: "ImageTransformer"))
+    }
+}
+
